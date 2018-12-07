2864e190191301baead7351e7738274d0373e377
Leandro <lefatecsis@gmail.com>
2018-12-07 00:09:09 -0200

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -1,28 +1,7 @@
 	
 
-<?php/*
+<?php
 
-// PARAMETROS PARA FAZER A CONEXÃO COM O banco
-
-$hostname = 'prosw.mysql.database.azure.com';
-$username = 'prosw@prosw';
-$userpwd = 'Azure2018';
-$databasename = 'prosw';
-
-// FAZENDO CONEXÃO COM O BD
-
-try
-	{
-	$dsn = 'mysql:host=prosw.mysql.database.azure.com';
-	$dbh = new PDO("mysql:host=prosw.mysql.database.azure.com; dbname=" . $databasename . ";", "prosw@prosw", "Azure2018");
-	}
-
-catch(PDOException $e)
-	{
-	echo 'ERROR: ' . $e->getMessage();
-	}
-
-*/?>
 private  $server = "mysql:host=prosw.mysql.database.azure.com;dbname=prosw";
  
 private  $user = "prosw@prosw";
@@ -66,4 +45,5 @@ public function closeConnection() {
 }
  
 ?>
+
 

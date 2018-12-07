af7314bb708fd497f50ef193e98b08d1904203a7
Leandro <lefatecsis@gmail.com>
2018-12-06 23:58:42 -0200

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -1,6 +1,6 @@
 	
 
-<?php
+<?php/*
 
 // PARAMETROS PARA FAZER A CONEXÃO COM O banco
 
@@ -22,5 +22,48 @@ catch(PDOException $e)
 	echo 'ERROR: ' . $e->getMessage();
 	}
 
+*/?>
+private  $server = "mysql:host=prosw.mysql.database.azure.com;dbname=prosw";
+ 
+private  $user = "prosw@prosw";
+ 
+private  $pass = "Azure2018";
+ 
+private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+ 
+protected $con;
+ 
+          	public function openConnection()
+ 
+           	{
+ 
+               try
+ 
+                 {
+ 
+	        $this->con = new PDO($this->server, $this->user,$this->pass,$this->options);
+ 
+	        return $this->con;
+ 
+                  }
+ 
+               catch (PDOException $e)
+ 
+                 {
+ 
+                     echo "There is some problem in connection: " . $e->getMessage();
+ 
+                 }
+ 
+           	}
+ 
+public function closeConnection() {
+ 
+   	$this->con = null;
+ 
+	}
+ 
+}
+ 
 ?>
 
